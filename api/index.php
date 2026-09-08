a9070b0163a35bcfe206e4378ae8dc017f013fff
Gading Aldien Nugraha <gadingtaiga@gmail.com>
2026-09-08 22:14:06 +0700

    Enable raw error display in index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,9 @@
 <?php
 
-echo "Tes Vercel Berhasil Jalan!";
-exit;
+// Paksa tampilkan semua error PHP ke layar
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// Panggil bootstrap Laravel bawaan
+require __DIR__ . '/../public/index.php';
